7ddc0cdaaadab92b05981b344746b6908950dfda
Evgeniy <tinkerbox@yandex.by>
2022-12-14 22:08:49 +0300

    File Upload, iFrame

---
+++ src/test/java/FrameTest.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FrameTest {
+
+    @Test
+    public void iFrameTest() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/iframe");
+
+        driver.switchTo().frame("mce_0_ifr");
+        WebElement frame = driver.findElement(By.id("tinymce"));
+        Assert.assertEquals(frame.getText(),"Your content goes here.");
+
+        driver.quit();
+    }
+}
