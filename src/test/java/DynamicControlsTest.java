9318d51931332794eabc81e8e90b7a9d8af6aa23
Evgeniy <tinkerbox@yandex.by>
2022-12-16 15:27:22 +0300

    DynamicControls

--- src/test/java/DynamicControlsTest.java
+++ src/test/java/DynamicControlsTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class dynamicControlsTest {
+public class DynamicControlsTest {
 
     @Test
     public void dynamicControlTest() {
@@ -42,6 +42,8 @@ public class dynamicControlsTest {
         buttons.get(1).click();
         wait.until(ExpectedConditions.elementToBeClickable(input));
         Assert.assertEquals(input.isEnabled(),true);
+
+        driver.quit();
 
     }
 
--- src/test/java/FrameTest.java
+++ src/test/java/FrameTest.java
@@ -23,6 +23,7 @@ public class FrameTest {
         driver.switchTo().frame("mce_0_ifr");
         WebElement frame = driver.findElement(By.id("tinymce"));
         Assert.assertEquals(frame.getText(),"Your content goes here.");
+        driver.switchTo().defaultContent();
 
         driver.quit();
     }
